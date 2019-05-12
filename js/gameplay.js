a89a4edc38d7b2d102125164ccc99d2bde531d76
josemwarrior <josemiguel.gs88@gmail.com>
2019-05-12 22:15:11 +0900

    Test basic level from array

--- js/gameplay.js
+++ js/gameplay.js
@@ -23,14 +23,32 @@ loader
 
 function setup() 
 {
+  let level_1 = [[2,-1,2,2,2,2,2],
+                [-1,2,-1,-1,-1,-1,2],
+                [2,-1,-1,-1,-1,-1,2],
+                [2,-1,-1,-1,-1,-1,2],
+                [2,-1,-1,-1,-1,-1,2],
+                [2,-1,-1,-1,-1,-1,2],
+                [2,2,2,2,2,2,2]];
   let spr_bg = new Sprite(resources["images/atlas.json"].textures["bg.png"]);
   spr_bg.x = 125;
   spr_bg.y = 373;
-  app.stage.addChild(spr_bg);
-  let white_block = new Sprite(resources["images/atlas.json"].textures["white_block.png"]);
-  white_block.x = 125;
-  white_block.y = 373;
-  app.stage.addChild(white_block);
-
+  //app.stage.addChild(spr_bg);
+  let start_x_position = 125;
+  let start_y_position = 373;
+  for(var x=0;x<7;++x)
+  {
+    for(var y=0;y<7;++y)
+    {
+      console.log(level_1[x][y]);
+      if (level_1[x][y]==2)
+      {
+        let white_block = new Sprite(resources["images/atlas.json"].textures["white_block.png"]);
+        white_block.x = start_x_position + (x * 56);
+        white_block.y = start_y_position + (y * 56);
+        app.stage.addChild(white_block);
+      }
+    }
+  }
 }
 
