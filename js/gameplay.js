ae0b8d85c4d0406ee2705bd5d486bce1b4997c97
josemwarrior <josemiguel.gs88@gmail.com>
2019-05-12 21:08:52 +0900

    Load images from atlas

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,8 +1,14 @@
+//Aliases
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
 //Create a Pixi Application
-let app = new PIXI.Application({width: 640, height: 1138});
+let app = new Application({width: 640, height: 1138});
 document.body.appendChild(app.view);
 app.renderer.autoResize = true;
-app.renderer.backgroundColor = 0x63E8EE;      
+app.renderer.backgroundColor = 0x0096FE;      
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 var scale = scaleToWindow(app.renderer.view);
@@ -11,14 +17,20 @@ window.addEventListener("resize", function(event){
 });  
 
 //Load sprites
-PIXI.loader.add("images/bg.png", {crossOrigin: true}).load(setup);
+loader
+  .add("images/atlas.json")
+  .load(setup);
 
 function setup() 
 {
-  let spr_bg = new PIXI.Sprite
-  (
-    PIXI.loader.resources["images/bg.png"].texture
-  );
+  let spr_bg = new Sprite(resources["images/atlas.json"].textures["bg.png"]);
+  spr_bg.x = 125;
+  spr_bg.y = 373;
   app.stage.addChild(spr_bg);
+  let white_block = new Sprite(resources["images/atlas.json"].textures["white_block.png"]);
+  white_block.x = 125;
+  white_block.y = 373;
+  app.stage.addChild(white_block);
+
 }
 
