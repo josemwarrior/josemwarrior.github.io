2ef778b24cb03e237d634ed28577ac6a20cc9add
josemwarrior <josemiguel.gs88@gmail.com>
2019-05-13 19:43:53 +0900

    Tint tiles

--- js/gameplay.js
+++ js/gameplay.js
@@ -6,8 +6,9 @@ let Application = PIXI.Application,
     tween = PIXI.tweenManager;
 
 //Define any variables that are used in more than one function
-let spr_player, current_level, spr_bg, spr_white_block, 
-state, player_x_array, player_y_array, number_steps;
+let spr_player, current_level, spr_bg, spr_white_block,
+    state, player_x_array, player_y_array, number_steps,
+    layer_tiles;
 //Capture the keyboard arrow keys
 let left = keyboard(37),
     up = keyboard(38),
@@ -15,16 +16,16 @@ let left = keyboard(37),
     down = keyboard(40);
 let can_move = true;
 
-
-
 const BACKGROUND_COLOR = 0x0096FE;
 const TILE_SIZE = 56;
 const PLAYER = 1;
 const WHITE_BLOCK = 2;
+const ORANGE_BLOCK = 0;
 const WIDTH_STAGE = 640;
 const HEIGHT_STAGE = 1138;
 const ATLAS_NAME = "images/atlas.json";
 const TEXTURE_WHITE_BLOCK = "white_block.png";
+const TEXTURE_ORANGE_BLOCK = "orange_block.png";
 const TEXTURE_BACKGROUND = "bg.png";
 const TEXTURE_PLAYER = "player.png";
 const OFFSET_X_LEVEL = 125;
@@ -32,7 +33,6 @@ const OFFSET_Y_LEVEL = 373;
 const TILES_X = 7;
 const TILES_Y = 7;
 const TIME_PER_TILE = 20;
-
 
 //Create a Pixi Application
 let app = new Application(
@@ -54,43 +54,20 @@ window.addEventListener("resize", function(event)
 //Load sprites
 loader
     .add(ATLAS_NAME)
-    .load(setup);
+    .load(start);
 
-function setup()
+function start()
 {
     current_level = 0;
     spr_bg = new Sprite(resources[ATLAS_NAME].textures[TEXTURE_BACKGROUND]);
     spr_bg.x = OFFSET_X_LEVEL;
     spr_bg.y = OFFSET_Y_LEVEL;
     app.stage.addChild(spr_bg);
-    for (var x = 0; x < TILES_X; ++x)
-    {
-        for (var y = 0; y < TILES_Y; ++y)
-        {
-            // White block
-            if (levels[current_level][x][y] == WHITE_BLOCK)
-            {
-                spr_white_block = new Sprite(resources[ATLAS_NAME].textures[TEXTURE_WHITE_BLOCK]);
-                spr_white_block.x = OFFSET_X_LEVEL + (x * TILE_SIZE);
-                spr_white_block.y = OFFSET_Y_LEVEL + (y * TILE_SIZE);
-                app.stage.addChild(spr_white_block);
-            }
-            // Player
-            if (levels[current_level][x][y] == PLAYER)
-            {
-                spr_player = new Sprite(resources[ATLAS_NAME].textures[TEXTURE_PLAYER]);
-                spr_player.x = OFFSET_X_LEVEL + (x * TILE_SIZE);
-                spr_player.y = OFFSET_Y_LEVEL + (y * TILE_SIZE);
-                app.stage.addChild(spr_player);
-                player_x_array = x;
-                player_y_array = y;
-            }
-        }
-    }
-    //Set the game state
-    state = play;
-
-    //arrow key `press` method
+    // Layer color tiles
+    layer_tiles = new PIXI.Container();
+    app.stage.addChild(layer_tiles);
+    print_level();
+    //keyboard events
     right.press = () =>
     {
         check_move_right();
@@ -107,17 +84,56 @@ function setup()
     {
         check_move_down();
     };
+    //Set the game state
+    state = play;
     //Start the game loop by adding the `gameLoop` function to
     //Pixi's `ticker` and providing it with a `delta` argument.
     app.ticker.add(delta => gameLoop(delta));
 }
 
+function print_level()
+{
+    for (var x = 0; x < TILES_X; ++x)
+    {
+        for (var y = 0; y < TILES_Y; ++y)
+        {
+            // White block
+            if (levels[current_level][x][y] == WHITE_BLOCK)
+            {
+                spr_white_block = new Sprite(resources[ATLAS_NAME].textures[TEXTURE_WHITE_BLOCK]);
+                spr_white_block.x = OFFSET_X_LEVEL + (x * TILE_SIZE);
+                spr_white_block.y = OFFSET_Y_LEVEL + (y * TILE_SIZE);
+                layer_tiles.addChild(spr_white_block);
+            }
+            // Player
+            if (levels[current_level][x][y] == PLAYER)
+            {
+                spr_player = new Sprite(resources[ATLAS_NAME].textures[TEXTURE_PLAYER]);
+                spr_player.x = OFFSET_X_LEVEL + (x * TILE_SIZE);
+                spr_player.y = OFFSET_Y_LEVEL + (y * TILE_SIZE);
+                app.stage.addChild(spr_player);
+                player_x_array = x;
+                player_y_array = y;
+            }
+        }
+    }
+
+    tint_orange_tile(player_x_array, player_y_array);
+}
+
+function tint_orange_tile(x, y)
+{
+    levels[current_level][x][y] = ORANGE_BLOCK;
+    var spr_orange_block = new Sprite(resources[ATLAS_NAME].textures[TEXTURE_ORANGE_BLOCK]);
+    spr_orange_block.x = OFFSET_X_LEVEL + (x * TILE_SIZE);
+    spr_orange_block.y = OFFSET_Y_LEVEL + (y * TILE_SIZE);
+    layer_tiles.addChild(spr_orange_block);
+}
+
 function gameLoop(delta)
 {
-
     //Move the cat 1 pixel 
     state(delta);
-
 }
 
 function play(delta)
@@ -131,18 +147,28 @@ function check_move_right()
     {
         can_move = false;
         number_steps = 0;
-        while(++player_x_array<TILES_X && levels[current_level][player_x_array][player_y_array]!=WHITE_BLOCK)
+        while (++player_x_array < TILES_X && levels[current_level][player_x_array][player_y_array] != WHITE_BLOCK)
         {
             number_steps++;
+            check_tile_colour(current_level, player_x_array, player_y_array, number_steps);
         }
         --player_x_array;
         var x_end = number_steps * TILE_SIZE;
         var tween_player = tween.createTween(spr_player);
-        tween_player.from({ x: spr_player.x }).to({ x: spr_player.x + x_end });
+        tween_player.from(
+        {
+            x: spr_player.x
+        }).to(
+        {
+            x: spr_player.x + x_end
+        });
         tween_player.time = TIME_PER_TILE * number_steps;
-        tween_player.on('end', () => { can_move = true; });
+        tween_player.on('end', () =>
+        {
+            can_move = true;
+        });
         tween_player.start();
-        if (number_steps==0) can_move = true;
+        if (number_steps == 0) can_move = true;
     }
 }
 
@@ -152,18 +178,28 @@ function check_move_down()
     {
         can_move = false;
         number_steps = 0;
-        while(++player_y_array<TILES_Y && levels[current_level][player_x_array][player_y_array]!=WHITE_BLOCK)
+        while (++player_y_array < TILES_Y && levels[current_level][player_x_array][player_y_array] != WHITE_BLOCK)
         {
             number_steps++;
+            check_tile_colour(current_level, player_x_array, player_y_array, number_steps);
         }
         --player_y_array;
         var y_end = number_steps * TILE_SIZE;
         var tween_player = tween.createTween(spr_player);
-        tween_player.from({ y: spr_player.y }).to({ y: spr_player.y + y_end });
+        tween_player.from(
+        {
+            y: spr_player.y
+        }).to(
+        {
+            y: spr_player.y + y_end
+        });
         tween_player.time = TIME_PER_TILE * number_steps;
-        tween_player.on('end', () => { can_move = true; });
+        tween_player.on('end', () =>
+        {
+            can_move = true;
+        });
         tween_player.start();
-        if (number_steps==0) can_move = true;
+        if (number_steps == 0) can_move = true;
     }
 }
 
@@ -173,18 +209,28 @@ function check_move_left()
     {
         can_move = false;
         number_steps = 0;
-        while(--player_x_array>=0&&levels[current_level][player_x_array][player_y_array]!=WHITE_BLOCK)
+        while (--player_x_array >= 0 && levels[current_level][player_x_array][player_y_array] != WHITE_BLOCK)
         {
             number_steps++;
+            check_tile_colour(current_level, player_x_array, player_y_array, number_steps);
         }
         ++player_x_array;
         var x_end = number_steps * TILE_SIZE;
         var tween_player = tween.createTween(spr_player);
-        tween_player.from({ x: spr_player.x }).to({ x: spr_player.x - x_end });
+        tween_player.from(
+        {
+            x: spr_player.x
+        }).to(
+        {
+            x: spr_player.x - x_end
+        });
         tween_player.time = TIME_PER_TILE * number_steps;
-        tween_player.on('end', () => { can_move = true; });
+        tween_player.on('end', () =>
+        {
+            can_move = true;
+        });
         tween_player.start();
-        if (number_steps==0) can_move = true;
+        if (number_steps == 0) can_move = true;
     }
 }
 
@@ -194,18 +240,52 @@ function check_move_up()
     {
         can_move = false;
         number_steps = 0;
-        while(--player_y_array<TILES_Y && levels[current_level][player_x_array][player_y_array]!=WHITE_BLOCK)
+        while (--player_y_array < TILES_Y && levels[current_level][player_x_array][player_y_array] != WHITE_BLOCK)
         {
             number_steps++;
+            check_tile_colour(current_level, player_x_array, player_y_array, number_steps);
         }
         ++player_y_array;
         var y_end = number_steps * TILE_SIZE;
         var tween_player = tween.createTween(spr_player);
-        tween_player.from({ y: spr_player.y }).to({ y: spr_player.y - y_end });
+        tween_player.from(
+        {
+            y: spr_player.y
+        }).to(
+        {
+            y: spr_player.y - y_end
+        });
         tween_player.time = TIME_PER_TILE * number_steps;
-        tween_player.on('end', () => { can_move = true; });
+        tween_player.on('end', () =>
+        {
+            can_move = true;
+        });
         tween_player.start();
-        if (number_steps==0) can_move = true;
+        if (number_steps == 0) can_move = true;
+    }
+}
+
+function check_tile_colour(current_level, x, y, steps)
+{
+    if (levels[current_level][x][y] != ORANGE_BLOCK)
+    {
+        var timer = {
+            x: 0
+        };
+        var tween_player = tween.createTween(timer);
+        tween_player.from(
+        {
+            x: 0
+        }).to(
+        {
+            x: 100
+        });
+        tween_player.time = TIME_PER_TILE * steps;
+        tween_player.on('end', () =>
+        {
+            tint_orange_tile(x, y)
+        });
+        tween_player.start();
     }
 }
 
@@ -252,10 +332,14 @@ function keyboard(keyCode)
 
 // Touch gestures
 let mc = new Hammer(app.view);
-mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+mc.get('pan').set(
+{
+    direction: Hammer.DIRECTION_ALL
+});
 
 // listen to events...
-mc.on("panleft panright panup pandown tap press", function(ev) {
+mc.on("panleft panright panup pandown tap press", function(ev)
+{
     //console.log(ev.type +" gesture detected.");
     if (ev.type == "panright")
     {
@@ -274,5 +358,3 @@ mc.on("panleft panright panup pandown tap press", function(ev) {
         check_move_down();
     }
 });
-
-
